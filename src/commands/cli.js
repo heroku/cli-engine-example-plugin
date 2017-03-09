23531cb874c6f3791a6844fcc7c3f5cdc10d1e23
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-08 18:38:46 -0800

    added some help

--- src/commands/cli.js
+++ src/commands/cli.js
@@ -5,8 +5,9 @@ import Command from 'cli-engine-command'
 export default class extends Command {
   static topic = 'cli'
   static command = 'test'
+  static help = 'this is an example command for cli-engine'
 
   run () {
-    this.log('ran cli:test')
+    this.log('ra cli:test')
   }
 }
