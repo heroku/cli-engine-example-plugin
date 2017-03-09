06c6406b253588774950d31479ada79409a45184
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-09 13:02:32 -0800

    updated example-plugin

--- src/commands/cli.test.js
+++ src/commands/cli.test.js
@@ -7,7 +7,6 @@
 import CLI from './cli'
 
 test('it runs the command', async () => {
-  let cli = new CLI({mock: true})
-  await cli._run()
+  let cli = await CLI.run([], {mock: true})
   expect(cli.stdout.output).toContain('ran cli:test')
 })
