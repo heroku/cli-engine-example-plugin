37bbd1da4f965fa595ac5deba6459fbd8ee20817
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-03-14 13:20:36 -0700

    remove debug statements

--- src/commands/duration.js
+++ src/commands/duration.js
@@ -8,10 +8,6 @@ export default class extends DurationMixin(Command) {
   static command = 'duration'
   static description = 'this is an example command showing duration parsing'
 
-  static flags = [
-    {name: 'foo'}
-  ]
-
   run () {
     this.log(`duration: ${this.duration}`)
   }
--- src/mixins/duration.js
+++ src/mixins/duration.js
@@ -15,12 +15,10 @@ declare class App extends Command {
 
 export default function <T: Class<Command>> (Base: T): $Shape<Class<App>> {
   return class DurationMixin extends Base {
-    static get flags (): Flag[] { return this._flags.concat([FLAG]) }
+    static get flags (): Flag[] { return super.flags.concat([FLAG]) }
     static set flags (flags: Flag[]) { this._flags = flags }
 
     get duration (): Date {
-      this.inspect(this.constructor.flags)
-
       // this is a little gross but I think needed to cast it to a string
       let duration = (((this.flags.duration): any): string)
       return new Date(duration)
