052a8f1fcffd41584f07b94424093849bc484a44
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-03-15 09:20:50 -0700

    fix mixin

--- src/commands/duration.js
+++ src/commands/duration.js
@@ -1,16 +1,18 @@
 // @flow
 
 import Command from 'cli-engine-command'
-import Duration from '../mixins/duration'
+import Duration, {DurationFlag} from '../mixins/duration'
 
 export default class extends Command {
   static topic = 'cli'
   static command = 'duration'
   static description = 'this is an example command showing duration parsing'
+  static flags = [DurationFlag]
 
   duration = new Duration(this)
 
   run () {
-    this.log(`duration: ${this.duration.toString()}`)
+    this.log('duration:')
+    this.inspect(this.duration.value)
   }
 }
