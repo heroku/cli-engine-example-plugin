24030bda1406d8959f20929044d455a75e23a4fe
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-03-15 09:15:31 -0700

    use new mixin syntax

--- src/commands/duration.js
+++ src/commands/duration.js
@@ -1,12 +1,14 @@
 // @flow
 
 import Command from 'cli-engine-command'
-import DurationMixin from '../mixins/duration'
+import Duration from '../mixins/duration'
 
-export default class extends DurationMixin(Command) {
+export default class extends Command {
   static topic = 'cli'
   static command = 'duration'
   static description = 'this is an example command showing duration parsing'
+
+  duration = new Duration(this)
 
   run () {
     this.log(`duration: ${this.duration.toString()}`)
