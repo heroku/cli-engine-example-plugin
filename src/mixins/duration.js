4c2a2b21140366896d0eb2c5b769d6700043f270
Matthew Origer <moriger@heroku.com>
2017-03-16 11:45:20 -0500

    example showing parsing of a custom flag

--- src/mixins/duration.js
+++ src/mixins/duration.js
@@ -13,10 +13,39 @@ export default class Duration {
   cmd: Command
   constructor (cmd: Command) {
     this.cmd = cmd
+    // setTimeout(this.cmd.reject, this.parse(this.cmd.flags.duration))
+  }
+  parse(arg : string){
+    let result = arg.match(/^(\d+) ?(ms|[sm]|milliseconds?|seconds?|minutes?)$/);
+    if (result) {
+      let magnitude = parseInt(result[1]);
+      let unit = result[2];
+      let multiplier = 1;
+      switch (unit) {
+        case 'ms':
+        case 'millisecond':
+        case 'milliseconds':
+          multiplier = 1;
+          break;
+        case 's':
+        case 'second':
+        case 'seconds':
+          multiplier = 1000;
+          break;
+        case 'm':
+        case 'minute':
+        case 'minutes':
+          multiplier = 1000 * 60;
+          break;
+        default:
+          // throw new Exception(`could not parse '${arg}'`)
+      }
+      return magnitude * multiplier;
+    }
   }
 
-  get duration (): Date {
-    let duration = this.cmd.flags.duration
-    return new Date(duration)
+  get duration (): number {
+    let duration = this.parse(this.cmd.flags.duration)
+    return Number(duration)
   }
 }
