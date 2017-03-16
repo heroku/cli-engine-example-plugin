690c7e521c968f54f6c794956d76fbc655dff1fb
Matthew Origer <moriger@heroku.com>
2017-03-16 14:11:08 -0500

    use promise race to demonstrate timing out

---
+++ src/commands/dynorestart.js
@@ -0,0 +1,29 @@
+// @flow
+
+import Command from 'cli-engine-command'
+import Duration from '../mixins/duration'
+import {FLAG as DurationFlag} from '../mixins/duration'
+
+export default class extends Command {
+  static topic = 'cli'
+  static command = 'dynorestart'
+  static description = 'this is an example command showing duration parsing. It takes five seconds to complete.'
+  static flags = [
+    DurationFlag
+  ]
+  duration = new Duration(this)
+
+  run() {
+    console.log('Restarting dynos. This will take five seconds. That is too long')
+    return Promise.race([this.duration.wait(), this.main()])
+  }
+
+  main() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('Done restarting dynos');
+        resolve()
+      }, 5000)
+    })
+  }
+}
