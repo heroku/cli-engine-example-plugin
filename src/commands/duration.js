a73fdd9a2cf46ff1377f667b32f58bde138fe447
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-03-17 21:26:03 -0700

    use new flag syntax

--- src/commands/duration.js
+++ src/commands/duration.js
@@ -1,18 +1,16 @@
 // @flow
 
 import Command from 'cli-engine-command'
-import Duration, {DurationFlag} from '../mixins/duration'
+import DurationFlag from '../flags/duration'
 
 export default class extends Command {
   static topic = 'cli'
   static command = 'duration'
   static description = 'this is an example command showing duration parsing'
-  static flags = [DurationFlag]
+  static flags = {duration: DurationFlag()}
 
-  duration = new Duration(this)
-
-  run () {
+  async run () {
     this.log('duration:')
-    this.inspect(this.duration.value)
+    this.inspect(this.flags.duration)
   }
 }
