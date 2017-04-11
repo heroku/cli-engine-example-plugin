02b7473458233723d9017233afaf96126a516f9f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-04-10 23:45:25 -0700

    standard fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // @flow
 
+import CLI from './commands/cli'
+
 export const topics = [
   {name: 'cli', description: 'example CLI engine topic'}
 ]
 
-import CLI from './commands/cli'
 export const commands = [CLI]
