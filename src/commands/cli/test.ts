62f862ada2fa3ecea92ffe735a02773ef9e4b221
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-26 23:49:26 -0800

    remove unneeded topic/command

--- src/commands/cli/test.ts
+++ src/commands/cli/test.ts
@@ -2,8 +2,6 @@ import { Command } from '@cli-engine/command'
 import { cli } from 'cli-ux'
 
 export default class extends Command {
-  static topic = 'cli'
-  static command = 'test'
   static description = 'this is an example command for cli-engine'
   static help = `it just prints some output
 
